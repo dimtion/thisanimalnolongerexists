8cecbd8e6f2afb6ed8eb0800ee8fb0f68a2b413c
Loïc Carr <zizou.xena@gmail.com>
2020-02-21 07:30:20 -0800

    Add analytics

--- src/main.rs
+++ src/main.rs
@@ -9,39 +9,47 @@ use rand::seq::IteratorRandom;
 
 use wantedspecies::*;
 
+struct AppState {
+    database: Species,
+    tracking_code: String,
+}
+
 #[derive(Template)]
 #[template(path = "index.html")]
 struct Index<'a> {
     specie: &'a Specie,
+    app: &'a AppState,
 }
 
 #[derive(Template)]
 #[template(path = "about.html")]
-struct About;
+struct About<'a> {
+    app: &'a AppState,
+}
 
 fn not_found() -> Result<HttpResponse> {
     // TODO: build a nice page
     Ok(HttpResponse::NotFound().body("404 NotFound"))
 }
 
-async fn about() -> Result<HttpResponse> {
-    let s = About.render().unwrap();
+async fn about(app: web::Data<AppState>) -> Result<HttpResponse> {
+    let s = About { app: &app }.render().unwrap();
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
-async fn index(database: web::Data<Species>, req: HttpRequest) -> Result<HttpResponse> {
+async fn index(app: web::Data<AppState>, req: HttpRequest) -> Result<HttpResponse> {
     let slug: String = req.match_info().query("slug").parse().unwrap();
     let specie = match slug.len() {
-        0 => match database.iter().choose(&mut rand::thread_rng()) {
+        0 => match app.database.iter().choose(&mut rand::thread_rng()) {
             Some(s) => s.1,
             None => panic!(),
         },
-        _ => match database.get(&slug) {
+        _ => match app.database.get(&slug) {
             Some(s) => s,
             None => return not_found(),
         },
     };
-    let s = Index { specie }.render().unwrap();
+    let s = Index { app: &app, specie }.render().unwrap();
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
@@ -60,9 +68,12 @@ async fn main() -> std::io::Result<()> {
     println!("Database size: {}", &database.len());
 
     HttpServer::new(move || {
-        let localdb = database.clone();
+        let app = AppState {
+            database: database.clone(),
+            tracking_code: env::var("TRACKING_CODE").or::<()>(Ok(String::from(""))).unwrap(),
+        };
         App::new()
-            .data(localdb)
+            .data(app)
             .service(fs::Files::new("/static", "static/"))
             .route("/about", web::get().to(about))
             .route("/{slug:[\\w-]*}", web::get().to(index))
